7a124b0ee7c66728e1c704af2450887b5f2f685e
7e-phart <7e-phart@users.noreply.github.com>
2026-09-07 21:33:02 +0200

    Force rebuild: add web-renderer flag to trigger fresh Vercel build

--- build.sh
+++ build.sh
@@ -33,6 +33,6 @@ flutter pub get
 echo "Lancement du build Flutter optimisé..."
 
 # Lancer le build Flutter avec optimisations
-flutter build web --release --tree-shake-icons
+flutter build web --release --tree-shake-icons --web-renderer canvaskit
 
 echo "Build terminé"
