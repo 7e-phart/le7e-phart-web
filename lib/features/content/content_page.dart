b810bef14c46cd56f6bcfe61db755a802ec160fd
7e-phart <7e-phart@users.noreply.github.com>
2026-09-08 21:40:50 +0200

    Fix build errors: add imageUrl field to EventModel and add url_launcher import

--- lib/features/content/content_page.dart
+++ lib/features/content/content_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:le7e_phart_app/services/content_service.dart';
 import 'package:le7e_phart_app/models/emission_model.dart';
 import 'package:le7e_phart_app/models/film_model.dart';
